5af0b01bac29b517ef4316b2e765a0a817182c9b
NabilSariff <82065451+NabilSariff@users.noreply.github.com>
2021-06-14 00:15:31 +0800

    Add files via upload

---
+++ views/inc/showcase.blade.php
@@ -0,0 +1,6 @@
+<div class="jumbotron text-center">
+    <div class="container">
+        <h1> Welcome to our Shoeniverse App! </h1>
+        <p class="lead" >Shoeniverse offers you the best quality and the latest fashion trend. Get the shoes you desire at an affordable price!</P>
+    </div>
+</div>
---
+++ views/inc/sidebar.blade.php
@@ -0,0 +1,11 @@
+@section('sidebar')   
+    <div class="well">
+        <h3>Shipped From</h3>
+        <li>Local</li>
+        <li>Overseas shoes</li>
+        <li>West Malaysia</li>
+        <li>East Malaysia</li>
+
+        
+
+    </div>
